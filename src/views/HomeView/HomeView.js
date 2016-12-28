be3f2f3bc0ba4f7ece85bd32a2362e9be3262873
gramulos <gramulos@gmail.com>
2016-12-28 14:08:09 +0400

    eslint configuration changed. Layout added.

---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Home } from '../../containers'
+
+class HomeView extends Component {
+  render () {
+    return <Home />
+  }
+}
+
+export default HomeView
